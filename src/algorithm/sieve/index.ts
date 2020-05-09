692b7ab5a39325e2c48b0bee7d9074b421d16b8f
zhouhaifei <zhouhaifei1994@sina.com>
2020-05-09 15:16:30 +0800

    feat(sieve): 增加埃拉托色尼算法

---
+++ src/algorithm/sieve/index.ts
@@ -0,0 +1,18 @@
+export function sieve(n: number): number[] {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+
+  for (let i = 2; i <= Math.floor(Math.pow(n, 0.5)); i++) {
+    if (result[i - 1] !== 0) {
+      let j = i * i;
+      while (j <= n) {
+        result[j - 1] = 0;
+        j += i;
+      }
+    }
+  }
+
+  return result.filter(n => n !== 0 && n !== 1);
+}
