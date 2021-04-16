489aff75c98c0dafc248890eb5563e52c0a14ed6
joey <joey@egenie.cn>
2021-04-16 23:14:46 +0800

    feat(algorithm): add maxProfitCountInfinity

--- src/algorithm/dp/maxProfit/maxProfitCountInfinity.ts
+++ src/algorithm/dp/maxProfit/maxProfitCountInfinity.ts
@@ -1,13 +1,15 @@
-export function maxProfitCount1(prices: number[]): number {
+export function maxProfitCountInfinity(prices: number[]): number {
   if (!prices.length) {
     return 0;
   }
 
   let dp_i_0 = 0;
   let dp_i_1 = -Infinity;
+  let tmp = 0;
   for (let i = 0; i < prices.length; i++) {
+    tmp = dp_i_0;
     dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
-    dp_i_1 = Math.max(dp_i_1, -prices[i]);
+    dp_i_1 = Math.max(dp_i_1, tmp - prices[i]);
   }
 
   return dp_i_0;
