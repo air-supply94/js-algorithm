f836ff486de42ed042348c925d4f9f2de4ccb3b1
joey <joey@egenie.cn>
2021-06-08 23:43:15 +0800

    feat(dataStructure/design): add nextGreaterElement nextGreaterElementCircle

---
+++ src/dataStructures/design/monotonicStack/monotonicStack.ts
@@ -0,0 +1,40 @@
+import { Stack } from '../../stack';
+
+export function nextGreaterElement(num1: number[], num2: number[]): number[] {
+  const stack = new Stack<number>();
+  const hashTable: {[key: number]: number; } = Object.create(null);
+
+  for (let i = num2.length - 1; i >= 0; i--) {
+    while (!stack.isEmpty() && stack.peek() <= num2[i]) {
+      stack.pop();
+    }
+
+    hashTable[num2[i]] = stack.isEmpty() ? -1 : stack.peek();
+    stack.push(num2[i]);
+  }
+
+  const result: number[] = Array(num1.length)
+    .fill(null);
+  for (let i = 0; i < num1.length; i++) {
+    result[i] = hashTable[num1[i]];
+  }
+  return result;
+}
+
+export function nextGreaterElementCircle(num: number[]): number[] {
+  const stack = new Stack<number>();
+  const len = num.length;
+  const result: number[] = Array(num.length)
+    .fill(null);
+
+  for (let i = 2 * len - 1; i >= 0; i--) {
+    while (!stack.isEmpty() && stack.peek() <= num[i % len]) {
+      stack.pop();
+    }
+
+    result[i % len] = stack.isEmpty() ? -1 : stack.peek();
+    stack.push(num[i % len]);
+  }
+
+  return result;
+}
