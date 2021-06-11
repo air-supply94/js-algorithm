69406564fc87b8918ecef93a4e2feea2677e6489
joey <joey@egenie.cn>
2021-06-11 21:17:12 +0800

    feat(algorithm/array): add removeElement

---
+++ src/algorithm/array/inplace/removeElement.ts
@@ -0,0 +1,12 @@
+export function removeElement(nums: number[], target: number): number {
+  let uniqueIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== target) {
+      nums[uniqueIndex] = nums[i];
+      uniqueIndex++;
+    }
+  }
+
+  return uniqueIndex;
+}
