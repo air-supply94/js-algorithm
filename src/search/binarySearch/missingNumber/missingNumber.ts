b1fdd2c2f702ca6279dbf78fe8aafbb8855f61e4
joey <joey@egenie.cn>
2021-07-02 18:31:55 +0800

    feat(bs/missingNumber): return start

--- src/search/binarySearch/missingNumber/missingNumber.ts
+++ src/search/binarySearch/missingNumber/missingNumber.ts
@@ -1,11 +1,8 @@
 export function missingNumber(nums: number[]): number {
-  if (nums.length <= 1) {
-    return 0;
-  }
-
   let start = 0;
   let end = nums.length - 1;
   let middle: number;
+
   while (start <= end) {
     middle = start + Math.floor((end - start) / 2);
     if (middle === nums[middle]) {
@@ -15,5 +12,5 @@ export function missingNumber(nums: number[]): number {
     }
   }
 
-  return nums[start] - 1;
+  return start;
 }
