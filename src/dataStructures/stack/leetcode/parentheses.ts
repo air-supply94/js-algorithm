a1f80f0f82522a884a8fa90735abd8290cc466f6
joey <joey@egenie.cn>
2021-07-13 21:40:35 +0800

    feat(stack): add minAddToMakeValid minInsertions

--- src/dataStructures/stack/leetcode/parentheses.ts
+++ src/dataStructures/stack/leetcode/parentheses.ts
@@ -1,6 +1,6 @@
 // https://leetcode-cn.com/problems/valid-parentheses/
 // 22
-export function validParentheses(str: string): boolean {
+export function isValid(str: string): boolean {
   const stack: string[] = [];
   const startMap = new Map<string, number>();
   const endMap = new Map<string, string>();
@@ -38,4 +38,50 @@ export function validParentheses(str: string): boolean {
   }
 
   return !stack.length;
+}
+
+// https://leetcode-cn.com/problems/minimum-add-to-make-parentheses-valid/
+// 921
+export function minAddToMakeValid(s: string): number {
+  let result = 0;
+  let need = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      need++;
+    } else {
+      need--;
+      if (need === -1) {
+        result++;
+        need = 0;
+      }
+    }
+  }
+
+  return result + need;
+}
+
+// https://leetcode-cn.com/problems/minimum-insertions-to-balance-a-parentheses-string/
+// 1541
+export function minInsertions(s: string): number {
+  let result = 0;
+  let need = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      need += 2;
+      if (need & 1) {
+        result++;
+        need--;
+      }
+    } else {
+      need--;
+      if (need === -1) {
+        result++;
+        need = 1;
+      }
+    }
+  }
+
+  return result + need;
 }
