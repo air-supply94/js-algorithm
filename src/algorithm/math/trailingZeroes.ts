eb19a1b548ee0fc45e76e610b72604188de89fa7
joey <joey@egenie.cn>
2021-07-28 23:30:21 +0800

    feat(algorithm/other): add masterMind

--- src/algorithm/math/trailingZeroes.ts
+++ src/algorithm/math/trailingZeroes.ts
@@ -1,11 +1,11 @@
 // https://leetcode-cn.com/problems/factorial-trailing-zeroes
 // 172
 export function trailingZeroes(n: number): number {
-  let result = 0;
-  let m = 5;
-  while (m <= n) {
-    result += Math.floor(n / m);
-    m *= 5;
+  let count = 0;
+  let x = n;
+  while (x) {
+    x = Math.floor(x / 5);
+    count += x;
   }
-  return result;
+  return count;
 }
