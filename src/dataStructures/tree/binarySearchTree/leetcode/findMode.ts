aa0d969c0672a81c643ec9260096ec6b1920955a
joey <joey@egenie.cn>
2021-08-14 00:52:09 +0800

    feat(dataStructure/bst): add findMode
    feat(dataStructure/bst): add minDiffInBST

---
+++ src/dataStructures/tree/binarySearchTree/leetcode/findMode.ts
@@ -0,0 +1,35 @@
+import { TreeNode } from './treeNode';
+
+// https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/
+// 501
+export function findMode(root: TreeNode | null): number[] {
+  let current = NaN;
+  let count = 0;
+  let maxCount = 0;
+  let result: number[] = [];
+
+  function dfs(tmpRoot: TreeNode) {
+    if (!tmpRoot) {
+      return;
+    }
+
+    dfs(tmpRoot.left);
+    if (tmpRoot.val === current) {
+      count++;
+    } else {
+      current = tmpRoot.val;
+      count = 1;
+    }
+
+    if (count === maxCount) {
+      result.push(current);
+    } else if (count > maxCount) {
+      maxCount = count;
+      result = [current];
+    }
+    dfs(tmpRoot.right);
+  }
+
+  dfs(root);
+  return result;
+}
---
+++ src/dataStructures/tree/binarySearchTree/leetcode/minDiffInBST.ts
@@ -0,0 +1,26 @@
+import { TreeNode } from './treeNode';
+
+// https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+// 783
+export function minDiffInBST(root: TreeNode | null): number {
+  let min = Infinity;
+  let pre: number | null = null;
+
+  function dfs(tmpRoot: TreeNode) {
+    if (!tmpRoot) {
+      return;
+    }
+
+    dfs(tmpRoot.left);
+    if (pre === null) {
+      pre = tmpRoot.val;
+    } else {
+      min = Math.min(tmpRoot.val - pre, min);
+      pre = tmpRoot.val;
+    }
+    dfs(tmpRoot.right);
+  }
+
+  dfs(root);
+  return min;
+}
