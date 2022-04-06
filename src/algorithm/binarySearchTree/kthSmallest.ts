03cbe8f66b97672f9c8e61fea55b8304bbe279ae
joey <joey@egenie.cn>
2022-04-07 07:56:25 +0800

    feat(bst): add kthSmallest

---
+++ src/algorithm/binarySearchTree/kthSmallest.ts
@@ -0,0 +1,24 @@
+import type { TreeNode } from './treeNode';
+
+// https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+export function kthSmallest(root: TreeNode | null, k: number): number {
+  let index = 0;
+  let result: number = null;
+
+  function dfs(node: TreeNode): void {
+    if (node == null || result !== null) {
+      return;
+    }
+
+    dfs(node.left);
+    index++;
+    if (index === k) {
+      result = node.val;
+      return;
+    }
+    dfs(node.right);
+  }
+
+  dfs(root);
+  return result;
+}
