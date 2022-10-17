9ac47ff3e1e4a2c796c43e651196a2fb6e270563
joey <joey@egenie.cn>
2022-10-17 13:08:15 +0800

    chore(*): 更新实现

--- src/algorithm/dp/maxProfit/maxProfitCountInfinity.ts
+++ src/algorithm/dp/maxProfit/maxProfitCountInfinity.ts
@@ -3,11 +3,11 @@
 export function maxProfitCountInfinity(prices: number[]): number {
   let dp_i_0 = 0;
   let dp_i_1 = -Infinity;
-  let tmp = 0;
+  let previous_dp_i_0 = 0;
   for (let i = 0; i < prices.length; i++) {
-    tmp = dp_i_0;
+    previous_dp_i_0 = dp_i_0;
     dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
-    dp_i_1 = Math.max(dp_i_1, tmp - prices[i]);
+    dp_i_1 = Math.max(dp_i_1, previous_dp_i_0 - prices[i]);
   }
 
   return dp_i_0;
--- src/algorithm/dp/maxProfit/maxProfitCountInfinityAndCoolDown.ts
+++ src/algorithm/dp/maxProfit/maxProfitCountInfinityAndCoolDown.ts
@@ -3,13 +3,15 @@
 export function maxProfitCountInfinityAndCoolDown(prices: number[]): number {
   let dp_i_0 = 0;
   let dp_i_1 = -Infinity;
+
+  // dp[i - 2][0]
   let previous = 0;
-  let tmp = 0;
+  let previous_dp_i_0 = 0;
   for (let i = 0; i < prices.length; i++) {
-    tmp = dp_i_0;
+    previous_dp_i_0 = dp_i_0;
     dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
     dp_i_1 = Math.max(dp_i_1, previous - prices[i]);
-    previous = tmp;
+    previous = previous_dp_i_0;
   }
 
   return dp_i_0;
