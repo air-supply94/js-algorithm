dc2ac91e5c1db0a04276f24d100a57479ff5f0d3
joey <joey@egenie.cn>
2022-10-19 08:14:14 +0800

    chore(*): 更新实现

--- src/algorithm/dp/classical/sequenceAndPalindrome/longestPalindromeSubsequence.ts
+++ src/algorithm/dp/classical/sequenceAndPalindrome/longestPalindromeSubsequence.ts
@@ -1,28 +1,20 @@
 // https://leetcode-cn.com/problems/longest-palindromic-subsequence/
 // 516
 export function longestPalindromeSubsequence(str: string): number {
-  if (str.length === 0) {
-    return 0;
-  }
-
-  const dp = Array(str.length)
-    .fill(null)
-    .map(() => Array(str.length)
-      .fill(0));
-
-  for (let i = 0; i < str.length; i++) {
-    dp[i][i] = 1;
-  }
+  const dp = Array(str.length).fill(1);
 
   for (let i = str.length - 1; i >= 0; i--) {
+    let previous = 0;
     for (let j = i + 1; j < str.length; j++) {
+      const tmp = previous;
+      previous = dp[j];
       if (str[i] === str[j]) {
-        dp[i][j] = dp[i + 1][j - 1] + 2;
+        dp[j] = tmp + 2;
       } else {
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+        dp[j] = Math.max(dp[j], dp[j - 1]);
       }
     }
   }
 
-  return dp[0][str.length - 1];
+  return dp[str.length - 1];
 }
--- src/algorithm/dp/classical/sequenceAndPalindrome/minPalindromeInsert.ts
+++ src/algorithm/dp/classical/sequenceAndPalindrome/minPalindromeInsert.ts
@@ -1,24 +1,20 @@
 // https://leetcode-cn.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
 // 1312
 export function minPalindromeInsert(str: string): number {
-  if (str.length === 0) {
-    return 0;
-  }
-
-  const dp = Array(str.length)
-    .fill(null)
-    .map(() => Array(str.length)
-      .fill(0));
+  const dp = Array(str.length).fill(0);
 
   for (let i = str.length - 1; i >= 0; i--) {
+    let previous = 0;
     for (let j = i + 1; j < str.length; j++) {
+      const tmp = previous;
+      previous = dp[j];
       if (str[i] === str[j]) {
-        dp[i][j] = dp[i + 1][j - 1];
+        dp[j] = tmp;
       } else {
-        dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+        dp[j] = Math.min(dp[j], dp[j - 1]) + 1;
       }
     }
   }
 
-  return dp[0][str.length - 1];
+  return dp[str.length - 1];
 }
