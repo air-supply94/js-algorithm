dd64818f29525ff44f1e809d964d1bb1522c48ea
joey <joey@egenie.cn>
2023-03-20 13:13:03 +0800

    chore(*): 更改实现

--- src/algorithm/math/fastPowering.ts
+++ src/algorithm/math/fastPowering.ts
@@ -3,11 +3,17 @@
 export function fastPowering(base: number, power: number): number {
   if (power === 0) {
     return 1;
-  } else if (power === 1) {
+  }
+
+  if (power === 1) {
     return base;
-  } else if (power === -1) {
+  }
+
+  if (power === -1) {
     return 1 / base;
-  } else if (power % 2 === 0) {
+  }
+
+  if ((power & 1) === 0) {
     const multiplier = fastPowering(base, power >> 1);
     return multiplier * multiplier;
   } else {
