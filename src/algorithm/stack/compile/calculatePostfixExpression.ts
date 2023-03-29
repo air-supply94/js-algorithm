40fd46ae04f31303a20ee74d8cdc32f343c4a00b
joey <joey@egenie.cn>
2023-03-29 18:32:42 +0800

    chore(*): 更改实现

--- src/algorithm/stack/compile/calculatePostfixExpression.ts
+++ src/algorithm/stack/compile/calculatePostfixExpression.ts
@@ -2,29 +2,25 @@
 // 150
 export function calculatePostfixExpression(tokens: string[]): number {
   const stack: number[] = [];
-  let num1: number = null;
-  let num2: number = null;
-  let token: string = null;
-  let tmp: number = null;
 
   for (let i = 0; i < tokens.length; i++) {
-    token = tokens[i];
+    const item = tokens[i];
 
-    if (token === '+') {
+    if (item === '+') {
       stack.push(stack.pop() + stack.pop());
-    } else if (token === '-') {
-      num2 = stack.pop();
-      num1 = stack.pop();
+    } else if (item === '-') {
+      const num2 = stack.pop();
+      const num1 = stack.pop();
       stack.push(num1 - num2);
-    } else if (token === '*') {
+    } else if (item === '*') {
       stack.push(stack.pop() * stack.pop());
-    } else if (token === '/') {
-      num2 = stack.pop();
-      num1 = stack.pop();
-      tmp = num1 / num2;
+    } else if (item === '/') {
+      const num2 = stack.pop();
+      const num1 = stack.pop();
+      const tmp = num1 / num2;
       stack.push(tmp > 0 ? Math.floor(tmp) : Math.ceil(tmp));
     } else {
-      stack.push(Number(token));
+      stack.push(Number(item));
     }
   }
 
