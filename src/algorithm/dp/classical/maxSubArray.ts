aa7ba84522e171e7e15231204c8e0bc2b9f8f736
joey <joey@egenie.cn>
2023-04-06 23:07:11 +0800

    feat(dp): 更新实现

--- src/algorithm/dp/classical/maxSubArray.ts
+++ src/algorithm/dp/classical/maxSubArray.ts
@@ -1,12 +1,12 @@
 // https://leetcode-cn.com/problems/maximum-subarray/
 // 53
 export function maxSubArray(numbers: number[]): number {
-  let pre = -Infinity;
+  let dp_i_0 = -Infinity;
   let result = -Infinity;
 
   for (let i = 0; i < numbers.length; i++) {
-    pre = Math.max(pre + numbers[i], numbers[i]);
-    result = Math.max(result, pre);
+    dp_i_0 = dp_i_0 < 0 ? numbers[i] : dp_i_0 + numbers[i];
+    result = result < dp_i_0 ? dp_i_0 : result;
   }
 
   return result;
@@ -15,35 +15,35 @@ export function maxSubArray(numbers: number[]): number {
 // https://leetcode-cn.com/problems/max-submatrix-lcci/
 // 金典-17.24
 export function getMaxMatrix(matrix: number[][]): number[] {
-  const h = matrix.length;
-  const w = matrix[0].length;
+  const height = matrix.length;
+  const width = matrix[0].length;
   const result = [
     0,
     0,
     0,
     0,
   ];
-  const dp = Array(w).fill(0);
   let startH = 0;
   let startW = 0;
+  const dp = Array(width).fill(0);
   let maxValue = -Infinity;
 
-  for (let i = 0; i < h; i++) {
+  for (let i = 0; i < height; i++) {
     dp.fill(0);
-    for (let j = i; j < h; j++) {
-      let pre = -Infinity;
-      for (let k = 0; k < w; k++) {
+    for (let j = i; j < height; j++) {
+      let dp_i_0 = -Infinity;
+      for (let k = 0; k < width; k++) {
         dp[k] += matrix[j][k];
-        if (pre + dp[k] < dp[k]) {
-          pre = dp[k];
+        if (dp_i_0 < 0) {
+          dp_i_0 = dp[k];
           startH = i;
           startW = k;
         } else {
-          pre += dp[k];
+          dp_i_0 += dp[k];
         }
 
-        if (pre > maxValue) {
-          maxValue = pre;
+        if (maxValue < dp_i_0) {
+          maxValue = dp_i_0;
           result[0] = startH;
           result[1] = startW;
           result[2] = j;
