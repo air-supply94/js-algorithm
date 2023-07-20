d91b4dcc339ffdc6af87548bf8d81f28d7d65134
joey <joey@egenie.cn>
2023-07-20 15:36:54 +0800

    chore(*): 更新二叉树说明

--- src/dataStructures/binarySearchTree/leetcode/serializePostAndInOrder.ts
+++ src/dataStructures/binarySearchTree/leetcode/serializePostAndInOrder.ts
@@ -2,11 +2,7 @@ import { TreeNode } from './treeNode';
 
 // https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
 // 106
-export function serializePostAndInOrder(inorder: number[], postorder: number[]): TreeNode | null {
-  return recursion(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1);
-}
-
-function recursion(postorder: number[], postStartIndex: number, postEndIndex: number, inorder: number[], inStartIndex: number, inEndIndex: number): TreeNode {
+export function serializePostAndInOrder(inorder: number[], postorder: number[], inStartIndex = 0, inEndIndex = inorder.length - 1, postStartIndex = 0, postEndIndex = postorder.length - 1): TreeNode {
   if (postStartIndex > postEndIndex) {
     return null;
   }
@@ -15,7 +11,7 @@ function recursion(postorder: number[], postStartIndex: number, postEndIndex: nu
   const equalIndex = inorder.findIndex((item) => item === rootValue, inStartIndex);
   const size = equalIndex - inStartIndex + 1;
   const root = new TreeNode(rootValue);
-  root.left = recursion(postorder, postStartIndex, postStartIndex + size - 2, inorder, inStartIndex, equalIndex - 1);
-  root.right = recursion(postorder, postStartIndex + size - 1, postEndIndex - 1, inorder, equalIndex + 1, inEndIndex);
+  root.left = serializePostAndInOrder(inorder, postorder, inStartIndex, equalIndex - 1, postStartIndex, postStartIndex + size - 2);
+  root.right = serializePostAndInOrder(inorder, postorder, equalIndex + 1, inEndIndex, postStartIndex + size - 1, postEndIndex - 1);
   return root;
 }
