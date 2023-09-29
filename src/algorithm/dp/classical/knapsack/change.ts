55d45bf405e208941d27c7258526e7e887229825
joey <joey@egenie.cn>
2023-09-29 16:44:26 +0800

    chore(*): 更新实现

--- src/algorithm/dp/classical/knapsack/change.ts
+++ src/algorithm/dp/classical/knapsack/change.ts
@@ -16,21 +16,3 @@ export function change(weight: number, weightList: number[]): number {
 
   return dp[weight];
 }
-
-/*
-export function change(weight: number, weightList: number[]): number {
-  const dp = Array(weight + 1)
-    .fill(0);
-  dp[0] = 1;
-
-  for (let i = 1; i <= weightList.length; i++) {
-    for (let w = 1; w <= weight; w++) {
-      if (w >= weightList[i - 1]) {
-        dp[w] += dp[w - weightList[i - 1]];
-      }
-    }
-  }
-
-  return dp[weight];
-}
-*/
