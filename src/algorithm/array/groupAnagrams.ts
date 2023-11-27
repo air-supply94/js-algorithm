811e3a33a520b94f514edbdc6a336480383d8be6
joey <joey@egenie.cn>
2023-11-27 10:05:42 +0800

    feat(*): 增加groupAnagrams

---
+++ src/algorithm/array/groupAnagrams.ts
@@ -0,0 +1,20 @@
+export function groupAnagrams(strs: string[]): string[][] {
+  const result: Record<string, string[]> = {};
+  const aCharCode = 'a'.charCodeAt(0);
+  const position: number[] = Array(26);
+
+  for (const str of strs) {
+    position.fill(0);
+    for (let j = 0; j < str.length; j++) {
+      position[str.charCodeAt(j) - aCharCode]++;
+    }
+    const positionStr = position.join();
+    if (result[positionStr]) {
+      result[positionStr].push(str);
+    } else {
+      result[positionStr] = [str];
+    }
+  }
+
+  return Object.values(result);
+}
