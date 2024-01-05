9899f1fc4af25442e7d099936260d4b526fba02f
joey <joey@egenie.cn>
2024-01-05 17:21:12 +0800

    feat(algorithm/array): add productExceptSelf

---
+++ src/algorithm/array/productExceptSelf.ts
@@ -0,0 +1,18 @@
+// https://leetcode.cn/problems/product-of-array-except-self/?envType=study-plan-v2&envId=top-100-liked
+// 238
+// top100
+
+export function productExceptSelf(nums: number[]): number[] {
+  const result = Array(nums.length).fill(1);
+  let prefix = 1;
+  let suffix = 1;
+
+  for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+    result[i] *= prefix;
+    result[j] *= suffix;
+    prefix *= nums[i];
+    suffix *= nums[j];
+  }
+
+  return result;
+}
