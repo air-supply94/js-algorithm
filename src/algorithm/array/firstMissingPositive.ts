9a1127295164aa835c50cf372d39eeb317f3f23c
zhouhaifei <zhouhaifei1994@sina.com>
2024-01-08 08:33:51 +0800

    feat(algorithm/array): add firstMissingPositive

---
+++ src/algorithm/array/firstMissingPositive.ts
@@ -0,0 +1,21 @@
+// https://leetcode.cn/problems/first-missing-positive/?envType=study-plan-v2&envId=top-100-liked
+// 41
+// top100
+export function firstMissingPositive(nums: number[]): number {
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[nums[i] - 1]) {
+      const tmp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = tmp;
+    }
+  }
+
+  for (let i = 0; i < n; ++i) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n + 1;
+}
