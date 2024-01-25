28001624c3ea480b3685c3efe78e3780bbc8b4ef
joey <joey@egenie.cn>
2024-01-25 14:35:35 +0800

    feat(*): add copyRandomList

---
+++ src/dataStructures/doubleLinkedList/leetcode/copyRandomList.ts
@@ -0,0 +1,41 @@
+// https://leetcode.cn/problems/copy-list-with-random-pointer/description/?envType=study-plan-v2&envId=top-100-liked
+// 138
+// top100
+
+class Node {
+  constructor(val?: number, next?: Node, random?: Node) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+    this.random = (random === undefined ? null : random);
+  }
+
+  public val: number;
+
+  public next: Node | null;
+
+  public random: Node | null;
+}
+
+export function copyRandomList(head: Node | null): Node | null {
+  if (head == null) {
+    return null;
+  }
+
+  const cache = new Map<Node, Node>();
+  let currentNode = head;
+  while (currentNode) {
+    const currentNewNode = new Node(currentNode.val);
+    cache.set(currentNode, currentNewNode);
+
+    currentNode = currentNode.next;
+  }
+
+  currentNode = head;
+  while (currentNode) {
+    cache.get(currentNode).next = cache.get(currentNode.next) || null;
+    cache.get(currentNode).random = cache.get(currentNode.random) || null;
+    currentNode = currentNode.next;
+  }
+
+  return cache.get(head);
+}
