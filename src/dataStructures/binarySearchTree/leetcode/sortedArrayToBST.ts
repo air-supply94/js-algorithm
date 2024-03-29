404f21be23bc0bc22338bdc4d42c5d0652c4ec42
zhouhaifei <zhouhaifei1994@sina.com>
2024-03-29 15:38:06 +0800

    chore(*): 更新实现

--- src/dataStructures/binarySearchTree/leetcode/sortedArrayToBST.ts
+++ src/dataStructures/binarySearchTree/leetcode/sortedArrayToBST.ts
@@ -3,14 +3,14 @@ import { TreeNode } from './treeNode';
 // https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
 // 108
 export function sortedArrayToBST(array: number[], start = 0, end = array.length - 1): TreeNode {
-  if (start <= end) {
-    const middle = start + Math.floor((end - start) / 2);
-    const node = new TreeNode(array[middle]);
-    node.left = sortedArrayToBST(array, start, middle - 1);
-    node.right = sortedArrayToBST(array, middle + 1, end);
-    return node;
-  } else {
+  if (start > end) {
     return null;
   }
+
+  const middle = start + Math.floor((end - start) / 2);
+  const node = new TreeNode(array[middle]);
+  node.left = sortedArrayToBST(array, start, middle - 1);
+  node.right = sortedArrayToBST(array, middle + 1, end);
+  return node;
 }
 
