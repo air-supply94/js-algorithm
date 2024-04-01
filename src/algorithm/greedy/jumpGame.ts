b75c8d28a55dc14b26f3a851b8bda2ce0c329b26
joey <joey@egenie.cn>
2024-04-01 10:06:00 +0800

    chore(*): 更新实现

--- src/algorithm/greedy/jumpGame.ts
+++ src/algorithm/greedy/jumpGame.ts
@@ -32,29 +32,3 @@ export function jump(numbers: number[]): number {
 
   return count;
 }
-
-// https://leetcode.cn/problems/partition-labels/description/
-// 763
-// top100
-export function partitionLabels(s: string): number[] {
-  const a = 'a'.charCodeAt(0);
-  const z = 'z'.charCodeAt(0);
-  const cache: number[] = Array(z - a + 1).fill(null);
-
-  for (let i = 0; i < s.length; i++) {
-    cache[s.charCodeAt(i) - a] = i;
-  }
-
-  const result: number[] = [];
-  let maxDistance = 0;
-
-  let start = 0;
-  for (let i = 0; i < s.length; i++) {
-    maxDistance = Math.max(maxDistance, cache[s.charCodeAt(i) - a]);
-    if (maxDistance === i) {
-      result.push(maxDistance - start + 1);
-      start = i + 1;
-    }
-  }
-  return result;
-}
